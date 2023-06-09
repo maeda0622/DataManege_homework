4ed96fb7c1801ede7210253e01e7e6a816374e02
maeda0622 <t20130622@gmail.com>
2023-06-09 10:37:17 +0900

    Dice game: initial revision

---
+++ Dice.py
@@ -0,0 +1,8 @@
+import random 
+
+for num in range(2):
+    Dice1 = random.randint(1,6)
+    Dice2 = random.randint(1,6)
+    
+    print("Rolling dice...")
+    print('Die %d : %d ' %(Dice1, Dice2))
